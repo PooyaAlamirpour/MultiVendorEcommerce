bfb396c5c51b0cc4ee5d08c1a9c64ba64a527d25
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2025-04-01 01:10:27 +0330

    Installing Shadcn UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-blue-500">Welcome to the Ecommerce Application</h1>
       <h1 className="text-blue-500 font-barlow">Welcome to the Ecommerce Application</h1>
+      <Button>Click Here</Button>
     </div>
   );
 }
